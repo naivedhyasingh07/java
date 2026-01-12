362996bd967b718c195d972c37bb6f265c1d43cb
naivedhyasingh07 <naivedhyasingh07@gmail.com>
2026-01-12 10:40:43 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Difference : " + (a - b));
+        System.out.println("Divide : " + (a / b));
+
+
+
+    }
+}
