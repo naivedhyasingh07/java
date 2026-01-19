c677de0c40e349e375c097263caade8663809851
naivedhyasingh07 <naivedhyasingh07@gmail.com>
2026-01-19 09:02:35 +0530

    Add files via upload

--- nameusingarray.java
+++ nameusingarray.java
@@ -1,8 +1,9 @@
 public class nameusingarray {
     public static void main(String[] args) {
         String[] names = {"NAivedhya", "Singh", "Makwana"};
-        for (String name : names) {
-            System.out.println(name);
+        for (int i = names.length - 1; i >= 0; i--) {
+            System.out.println(names[i]);
         }
     }
 }
+
