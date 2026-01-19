0e1be3b30e0d9bd0ae1ff7d3db68ad2e23ced787
naivedhyasingh07 <naivedhyasingh07@gmail.com>
2026-01-19 09:03:01 +0530

    Add files via upload

---
+++ divide.java
@@ -0,0 +1,15 @@
+public class divide {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed");
+        } else {
+            int result = a / b;
+            System.out.println("Result = " + result);
+        }
+    }
+}
+
+    
